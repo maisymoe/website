132a8bb789fa34183cf803a8596264da55784208
maisy <maisymoe@riseup.net>
2024-02-06 23:53:37 +0000

    [Global] The departure
    * [Refresh] Initial work
    
    * [Pages > About] The prior commit was rushed and it shows
    
    * [Layouts > BaseLayout] Update embed colour
    
    * [Pages > About] Add description
    
    * [Global] The departure

--- src/def.d.ts
+++ src/def.d.ts
@@ -3,14 +3,6 @@ interface Route {
     path: string;
 }
 
-interface Social {
-    platform: string;
-    username: string;
-    href: string;
-    iconPack?: string;
-    color?: string;
-}
-
 interface Post {
     title: string;
     description: string;
--- src/pages/blog.rss.ts
+++ src/pages/blog.rss.ts
@@ -2,7 +2,7 @@ import getRSS, { pagesGlobToRssItems } from "@astrojs/rss";
 
 export const GET = async () => await getRSS({
     title: "The Room of Rain",
-    description: "maisy's blog - a rainy place for rambling, coding and more.",
+    description: "maisy's blog - no longer active",
     site: new URL("blog/", import.meta.env.SITE).href,
     items: await pagesGlobToRssItems(import.meta.glob("./blog/**/*.mdx")),
 });
